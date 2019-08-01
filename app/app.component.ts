20e00330e1f77c2eab100692af21a256aff8cf5f
786hasan <rony_hasan@yahoo.com>
2019-07-31 21:12:03 -0500

    scasf
    asfd

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+//added test
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'icode';
+}
